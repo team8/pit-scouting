f02f5449f0d748fab9a84b67a4e4d0b20f34701b
Charger1256A <aditya.ram112005@gmail.com>
2022-05-21 02:32:17 -0700

    filename change

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = 'https://vdmylnxrkgtjxfwxmjsh.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkbXlsbnhya2d0anhmd3htanNoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MjUxNjUwNiwiZXhwIjoxOTY4MDkyNTA2fQ.Uqt4qjG_LQBLdCwvVVWtQulIYHlpClMttETw2O7UZ6I"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkbXlsbnhya2d0anhmd3htanNoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MjUxOTg4OSwiZXhwIjoxOTY4MDk1ODg5fQ.njUwNleu2UDsWg-H_JhPxfCnMm7b-LAeom4HolypLDk"
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
